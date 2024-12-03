3a2021355398de0339521497309b467b2733b1d5
DiamondHands89 <kwamemullins@outlook.com>
2024-12-03 16:15:34 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,43 @@
+// I. Variables & Datatypes
+    // A. Q + A
+        // 1. How do we assign a value to a variable?
+            // We use the assignment operator (=).
+            // let x = 3
+
+        // 2. How do we change the value of a variable?
+            // We use the assignment operator again (=)... assuming we didn't use const to assign it in the first place.
+            // let x = 5
+
+        // 3. How do we assign an existing variable to a new variable?
+            // let agent = "DiamondHands"
+            // let codeName = agent
+
+        // 4. Remind me, what are declare, assign, and define?
+            // Declare creates the variable: let x
+            // Assign gives the variable a value: x = 10
+            // Define is both together: let x = 10
+
+        // 5. What is pseudocoding and why should you do it?
+            // Planning out your code in human language as opposed to computer language. It's easier for the humans to read.
+
+        // 6. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+            // 70%-30%
+
+
+
+    // B. Strings
+        //For all other questions that involve writing code, you can solve them via the following instructions.
+            
+        //Create a variable called firstVariable
+            // Assign it the value of the string "Hello World"
+            
+            // Change the value of this variable to some number
+            
+            // Store the value of firstVariablein a new variable called secondVariable
+            
+            // Change the value of secondVariableto any string.
+            
+            // What is the value of firstVariable?
+            
+            // Create a variable called yourNameand set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourNameso that it returns a new string with them concatenated.
+            // ex: Hello, my name is Jean Valjean
